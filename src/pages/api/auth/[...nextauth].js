2718973eef9cea73fc92c28d5820f6d7aa70df9d
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2025-05-05 06:41:55 -0400

    fixing issuses using gh pages.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,14 +1,14 @@
-    // pages/api/auth/[...nextauth].js
-    import NextAuth from "next-auth";
-    import Providers from "next-auth/providers";
-    
-    export default NextAuth({
-      providers: [
-        Providers.GitHub({
-          clientId: process.env.GITHUB_CLIENT_ID,
-          clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
-        // Add more providers as needed
-      ],
-      // ... other configurations
-    });
+// pages/api/auth/[...nextauth].js
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    // Add more providers as needed
+  ],
+  // ... other configurations
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import {v4 as uuid} from 'uuid';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import useSWR from 'swr'; // Example of importing a node module
+
 const Index = () => <App />
 
 export default Index;
