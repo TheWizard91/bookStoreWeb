1208c9d7420ac9704bc6a22f113f1865df42d8f1
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2025-05-05 07:23:04 -0400

    fixing issuses with deployment.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,13 +2,13 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
-export default NextAuth({
-  providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    // Add more providers as needed
-  ],
-  // ... other configurations
-});
+// export default NextAuth({
+//   providers: [
+//     Providers.GitHub({
+//       clientId: process.env.GITHUB_CLIENT_ID,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+//     }),
+//     // Add more providers as needed
+//   ],
+//   // ... other configurations
+// });
